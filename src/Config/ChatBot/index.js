287ef996a05c56ef373027452269f6493172e04a
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-08-17 16:02:52 -0300

    add iframe content

--- src/Config/ChatBot/index.js
+++ src/Config/ChatBot/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChatBot from 'react-simple-chatbot';
+import styled from 'styled-components';
+
+const Main = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+`
+
+const Iframe = styled.iframe`
+    border-radius: 10px;
+`
 
 const repositorios = [
     {
         id: '1',
         name: 'Previsão do tempo',
         url: 'https://weather-woad.vercel.app/',
-    }, 
-    { 
-        id: '2', 
-        name: 'MendesFlix', 
+    },
+    {
+        id: '2',
+        name: 'MendesFlix',
         url: 'https://mendesflix.vercel.app'
     }
 ]
@@ -37,22 +47,45 @@ const steps = [
     {
         id: '4',
         message: 'Estou gerando a página...',
-        end: true,
-    }
+        trigger: 5
+    },
+    {
+        id: '5',
+        message: 'Feito!',
+        end: true
+    },
 ]
+
+var url = "";
 
 function Chat() {
 
-    function handleEnd({values}){
-       
+    const [page, setPage] = useState(false);
+
+    function handleEnd({ values }) {
+        setPage(true);
+        url = values[0];
     }
 
     return (
-        <ChatBot
-            handleEnd = {handleEnd}
-            steps={steps}
-        />
-    ) 
+        <Main>
+            <ChatBot
+                handleEnd={handleEnd}
+                steps={steps}
+            />
+            {page ?
+                <Iframe
+                    src={url}
+                    title={url}
+                    width="100%"
+                    allowfullscreen="allowfullscreen"
+                /> :
+                <>
+                </>
+            }
+        </Main>
+
+    )
 }
 
 export default Chat;
